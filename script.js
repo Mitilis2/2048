5b27d5e37869af580f70fafd40157c745f3e76ff
Mitul Shah <mitulshah91@gmail.com>
2021-10-19 21:15:21 -0400

    add winner/game over message

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', () =>{
     const gridDisplay = document.querySelector('.grid')
     const scoreDisplay = document.getElementById('score')
     const resultDisplay = document.getElementById('result')
-    const widths = 4;
+    const width = 4;
     let squares = []
     
     
     // create the play area
 
     function createBoard() {
-     for (let i = 0; i < widths * widths; i++) {
+     for (let i = 0; i < width * width; i++) {
            let square = document.createElement('div')
             square.innerHTML = 0
             gridDisplay.appendChild(square)
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     }
 
  
-// swipe left
+    // swipe left
 
 function leftSwipe(){
     for(let i = 0; i < 16; i++){
@@ -77,6 +77,57 @@ function leftSwipe(){
         }
     }
 }
+    // swipe down
+
+    function downSwipe(){
+        for(let  i = 0; i < 4; i++){
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i+width].innerHTML
+            let totalThree = squares[i + (width*2)].innerHTML
+            let totalFour = squares[i + (width*3)].innerHTML
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            let filteredColumn = column.filter(num => num)
+            let missingElement = 4 - filteredColumn.length
+            let zeros = Array(missingElement).fill(0)
+            let newColumn = zeros.concat(filteredColumn)
+
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i+width].innerHTML = newColumn[1]
+            squares[i + (width*2)].innerHTML = newColumn[2]
+            squares[i + (width*3)].innerHTML = newColumn[3]
+
+        }
+    }
+
+    // swipe up
+
+    
+    function upSwipe(){
+        for(let  i = 0; i < 4; i++){
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i+width].innerHTML
+            let totalThree = squares[i + (width*2)].innerHTML
+            let totalFour = squares[i + (width*3)].innerHTML
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            let filteredColumn = column.filter(num => num)
+            let missingElement = 4 - filteredColumn.length
+            let zeros = Array(missingElement).fill(0)
+            let newColumn = filteredColumn.concat(zeros)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i+width].innerHTML = newColumn[1]
+            squares[i + (width*2)].innerHTML = newColumn[2]
+            squares[i + (width*3)].innerHTML = newColumn[3]
+
+        }
+    }
+
+
+    // combine numbers
+
 
     function combineRowNumbers(){
         for(let i = 0; i < 15; i++){
@@ -86,6 +137,20 @@ function leftSwipe(){
                 squares[i+1].innerHTML = 0
             }
         }
+        winnerCheck()
+        gameOver()
+    }
+
+    function combineColumnNumbers(){
+        for(let i = 0; i < 12; i++){
+            if(squares[i].innerHTML === squares[i + width].innerHTML){
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+1].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i + width].innerHTML = 0
+            }
+        }
+        winnerCheck()
+        gameOver()
     }
 
     // keyboard functionality
@@ -95,6 +160,10 @@ function leftSwipe(){
             keyRight()
         } else if(e.keyCode === 37){
             keyLeft()
+        } else if(e.keyCode === 38){
+            keyUp()
+        } else if (e.keyCode === 40){
+            keyDown()
         }
     }
     document.addEventListener('keyup', control)
@@ -114,13 +183,45 @@ function keyLeft(){
 }
  
 
+    function keyDown(){
+        downSwipe()
+        combineColumnNumbers()
+        downSwipe()
+        generateNumber()
+    }
+
+    function keyUp(){
+        upSwipe()
+        combineColumnNumbers()
+        upSwipe()
+        generateNumber()
+    }
+
+    //  check win
+
+    function winnerCheck(){
+        for (let i = 0; i < squares.length; i++){
+            if(squares[i].innerHTML == 64){
+                resultDisplay.innerHTML = 'You Won The Game!'
+                document.removeEventListener('keyup', control)
+            }
+        }
+    }
 
 
+    // zero zeros check
 
-
-
-
-
+    function gameOver(){
+        let zeros = 0
+        for (let i = 0; i < squares.length; i++){
+            if(squares[i].innerHTML == 0){
+                zeros++
+            }
+        }
+        if(zeros === 0) {
+            resultDisplay.innerHTML = "You Lost!"
+        }
+    }
 
 
 
